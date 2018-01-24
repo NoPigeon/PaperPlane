a233c553e6a34a7ec0f82fb3e36acdcaf4774af8
Yang Liu <lykavin@hotmail.com>
2018-01-24 21:09:22 +0800

    last commit before update scene

---
+++ Assets/Scripts/VRFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRFollow : MonoBehaviour {
+
+    public Transform vrPivot;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = vrPivot.position;
+        transform.rotation = vrPivot.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = vrPivot.position;
+        transform.forward = vrPivot.forward;
+	}
+}
