74c06a48b6d91cf3514d4dfc860c62913fc2eefa
Yang Liu <lykavin@hotmail.com>
2018-01-25 15:27:49 +0800

    added wind sound

---
+++ Assets/Scripts/wow/PlaneSound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneSound : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+
+
+    float count;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector3 v = rb.velocity;
+        v = transform.InverseTransformVector(v);
+
+        Debug.Log(v);
+
+       //  v = new Vector3(0f, 0f, v.z);
+
+        float pp = v.magnitude;
+
+        pp = pp / speed;
+        if (pp > 1)
+        {
+            pp = 1;
+        }
+        rb.gameObject.GetComponent<AudioSource>().volume = (float)pp;
+	}
+
+}
